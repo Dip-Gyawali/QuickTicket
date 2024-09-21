88b5000f5ef06656700cbfa10f68b3999a4372a5
Hiten <hitengamer11@gmail.com>
2024-09-21 23:53:27 +0545

    styling done

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
+    <nav>
       <h1>Quick-Ticket</h1>
       <Link href='/'>Dashobard</Link>
       <Link href='/tickets'>Tickets</Link>
-    </div>
+    </nav>
   )
 }
