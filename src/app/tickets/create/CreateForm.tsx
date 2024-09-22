133520327d8771d12accf226072a1b9bd1484357
Hiten <hitengamer11@gmail.com>
2024-09-22 12:22:37 +0545

    completed

--- src/app/tickets/create/CreateForm.tsx
+++ src/app/tickets/create/CreateForm.tsx
@@ -1,19 +1,44 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from "react";
 
 export default function CreateForm() {
-   const [title,setTitle] = useState<string>('');
-   const [body,setBody] = useState<string>('');
-   const [priority,setPriority] = useState<string>('low');
-   const [isLodaing,setisLoading] = useState<boolean>(false)
+  const router = useRouter();
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+  const [priority, setPriority] = useState<string>("low");
+  const [isLodaing, setisLoading] = useState<boolean>(false);
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setisLoading(true);
+
+    const ticket = {
+      title,
+      body,
+      priority,
+      user_email: "hiten@gmail.com",
+    };
+
+    const res = await fetch("http://localhost:5000/tickets", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(ticket),
+    });
+
+    if (res.status === 201) {
+      router.refresh();
+      router.push("/tickets");
+    }
+  }
 
   return (
-    <form className="w-1/2">
+    <form onSubmit={handleSubmit} className="w-1/2">
       <label>
         <span>Title:</span>
         <input
-          required 
+          required
           type="text"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
@@ -29,15 +54,16 @@ export default function CreateForm() {
       </label>
       <label>
         <span>Priority:</span>
-        <select 
-          onChange={(e) => setPriority(e.target.value)}
-          value={priority}
-        >
+        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
           <option value="low">Low Priority</option>
           <option value="medium">Medium Priority</option>
           <option value="high">High Priority</option>
         </select>
       </label>
+      <button className="btn-primary" disabled={isLodaing}>
+        {isLodaing && <span>Adding..</span>}
+        {!isLodaing && <span>Add new ticket</span>}
+      </button>
     </form>
-  )
+  );
 }
--- src/app/tickets/create/page.tsx
+++ src/app/tickets/create/page.tsx
@@ -1,8 +1,10 @@
+import CreateForm from "./CreateForm";
+
 export default function Createticket(){
     return (
         <main>
             <h1 className="text-center">Open new Ticket</h1>
-
+             <CreateForm/>
         </main>
     )
 }
